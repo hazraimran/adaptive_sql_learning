2b83c52b79007878e305f1043cce56c140d1cc4e
Yan Wang <wang.yan8@northeastern.edu>
2025-12-05 22:12:15 -0800

    Update default port in server configuration

--- backend/server.py
+++ backend/server.py
@@ -261,5 +261,5 @@ def end_session():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 3001))  
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port)
